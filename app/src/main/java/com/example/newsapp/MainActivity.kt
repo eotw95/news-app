5d70a5852da002fe25c8c5082d4768156139ca36
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-09-27 22:45:49 +0900

    Add calling JsoupClient API

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.newsapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,10 +13,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.newsapp.ui.theme.NewsAppTheme
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.util.Optional
+import kotlin.streams.toList
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Thread {
+            HtmlParser.get()
+        }.start()
         setContent {
             NewsAppTheme {
                 // A surface container using the 'background' color from the theme
