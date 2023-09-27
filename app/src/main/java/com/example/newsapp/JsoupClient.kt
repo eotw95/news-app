e6160acb5350dae05f4015858f2f3c096a254f0f
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-09-27 22:44:25 +0900

    Update JsoupClient

--- app/src/main/java/com/example/newsapp/JsoupClient.kt
+++ app/src/main/java/com/example/newsapp/JsoupClient.kt
@@ -1,4 +1,16 @@
 package com.example.newsapp
 
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.util.Optional
+
 class JsoupClient {
+    companion object {
+        fun fromUrl(url: String): Optional<Document>{
+            val result = Jsoup.connect(url).get()
+            return Optional.ofNullable(result)
+        }
+    }
 }
